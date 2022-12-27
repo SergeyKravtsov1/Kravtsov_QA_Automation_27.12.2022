e0bfd63f9a168c4f6009fd4967e6037bf3e568a8
Sergey Kravtsov <sergeykravtsovkharkiv@gmail.com>
2022-12-28 00:07:41 +0200

    Attack stats

---
+++ src/homework5/main.java
@@ -0,0 +1,21 @@
+package homework5;
+
+public class main {
+    public static void main(String[] args) {
+
+        int a = 13;
+        int b = 24;
+        int c = 46;
+
+        double d = 9;
+        double e = 35;
+        double f = 12;
+
+        int Li = (a + b +c)*860;
+        double Mi = (d + e + f)*860*1.5;
+
+        System.out.println(Li);
+        System.out.println(Mi);
+
+    }
+}
