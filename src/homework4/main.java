56d6cc9a17084022cbb73fc254766c231ee6c312
Sergey Kravtsov <sergeykravtsovkharkiv@gmail.com>
2022-12-27 23:07:57 +0200

    parallelepiped

---
+++ src/homework4/main.java
@@ -0,0 +1,22 @@
+package homework4;
+
+public class main {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 7;
+        int c = 9;
+
+        int l = a;
+        int w = b;
+        int h = c;
+
+        int volume = l * w * h;
+        System.out.println("Volume of the parallelepiped = " + volume);
+
+        int lenght = l + w + h;
+        System.out.println(lenght);
+
+
+    }
+}
