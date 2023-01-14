66c3356ce35b82e8671f6a859a0bbbffe104f519
Sergey Kravtsov <sergeykravtsovkharkiv@gmail.com>
2023-01-14 11:45:10 +0200

    Person

---
+++ src/Homework11/Person.java
@@ -0,0 +1,29 @@
+package Homework11;
+
+public class Person {
+    public static void main(String[] args) {
+
+        personInfo1("Will", "Smith", "New York", "2936729");
+        personInfo2("Jackie", "Chan", "Shanghai", "12312412412");
+        personInfo3("Sherlock", "Holmes", "London", "37742123513");
+
+
+    }
+
+    static void personInfo1(String name, String surname, String town, String telephone) {
+        System.out.println(name + " " + surname + " " + town + " " + telephone);
+
+    }
+
+    static void personInfo2(String name, String surname, String town, String telephone) {
+        System.out.println(name + " " + surname + " " + town + " " + telephone);
+
+    }
+
+    static void personInfo3(String name, String surname, String town, String telephone) {
+        System.out.println(name + " " + surname + " " + town + " " + telephone);
+        {
+
+        }
+    }
+}
